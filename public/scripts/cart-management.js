c922e1c1d34e443a47011480d0881427765f20d2
Dhruv Jain <dhruvjain657@gmail.com>
2023-05-28 05:28:58 +0530

    cart updated

---
+++ public/scripts/cart-management.js
@@ -0,0 +1,40 @@
+const addTOCartButtonElement = document.querySelector('#product-details button');
+const cartBadgeElement = document.querySelector('.nav-items .badge');
+
+async function addToCart()
+{
+    const productId = addTOCartButtonElement.dataset.productid;
+    const csrfToken = addTOCartButtonElement.dataset.csrf;
+
+    let response;
+    try {
+    response = await fetch('/cart/items', {
+        method: 'POST',
+        body: JSON.stringify({
+            productId: productId,
+            _csrf: csrfToken
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    } catch(error) 
+    {
+        alert('Something went wrong!');
+        return;
+    }
+
+    if(!response.ok)
+    {
+        alert('Something went wrong!');
+        return;
+    }
+
+    const responseData = await response.json();
+
+    const newTotalQuantity = responseData.newTotalItems;
+
+    cartBadgeElement.newTotalItems = newTotalQuantity;
+}
+
+addTOCartButtonElement.addEventListener('click', addToCart);
