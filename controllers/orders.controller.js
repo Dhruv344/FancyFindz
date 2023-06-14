1be553f6e75229389ee273fa640f8e13225af668
Dhruv Jain <dhruvjain657@gmail.com>
2023-06-14 23:07:12 +0530

    "Error updated"

--- controllers/orders.controller.js
+++ controllers/orders.controller.js
@@ -53,7 +53,7 @@ async function addOrder(req, res, next)
         {
             return {
                 price_data: {
-                    currency: 'usd',
+                    currency: 'inr',
                     product_data: {
                         name: item.product.title,
                     },
